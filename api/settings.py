a07e2b8c6c0c19ece11dc262040f9206f7fcd783
Ashwinpounikar <135624363+Ashwinpounikar@users.noreply.github.com>
2024-08-22 18:05:11 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -161,7 +161,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rock-8.onrender.com', 'rock-7.onrender.com']
+
 
 # Application definition
 INSTALLED_APPS = [
